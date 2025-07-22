1ef72c7fe1dd8f02eae612695a46b66bc6f92f20
Eduardo Pires <dudupiresrosa@gmail.com>
2025-07-22 17:32:25 -0300

    ✨ Add validation for .env
    Adicionado um package que valida e tipa os valores do .env

---
+++ packages/env/index.ts
@@ -0,0 +1,20 @@
+import { createEnv } from '@t3-oss/env-core'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.url(),
+    POSTGRES_USER: z.string(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DB: z.string(),
+  },
+  runtimeEnv: {
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_DB: process.env.POSTGRES_DB,
+  },
+  emptyStringAsUndefined: true,
+})
