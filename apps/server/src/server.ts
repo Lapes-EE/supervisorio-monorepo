1d96d14236d93d4cec8c467553c6a3c1ba95c6b3
Eduardo Pires <dudupiresrosa@gmail.com>
2025-09-10 15:35:06 -0300

    🐛 Git merge fix
    #

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -6,11 +6,7 @@ import { startTelemetryCollector } from './services/telemetry-collector'
 api
   .listen({ port: env.PORT, host: '0.0.0.0' })
   .then(() => {
-<<<<<<< HEAD
     if (env.NODE_ENV === 'on') {
-=======
-    if (env.NODE_ENV !== 'on') {
->>>>>>> 5af8209c3597a75e468e3687b62b10a3684a9ca2
       startTelemetryCollector()
       startDisabledMetersRetry()
     }
