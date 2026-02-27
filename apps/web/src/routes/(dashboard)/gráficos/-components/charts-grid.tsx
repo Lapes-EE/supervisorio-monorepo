d7e16e0404f5acd063f3f7551b9c6d1342459f83
EuduardoP <dudupiresrosa@gmail.com>
2026-02-27 20:48:18 -0300

    feat(graphs): add charts grid component

---
+++ apps/web/src/routes/(dashboard)/gráficos/-components/charts-grid.tsx
@@ -0,0 +1,21 @@
+import type { GetTelemetry200DataItem } from '@/http/gen/model/get-telemetry200-data-item.gen'
+import { CurrentChart } from './charts/current-chart'
+import { PowerChart } from './charts/power-chart'
+import { PowerFactorChart } from './charts/power-factor-chart'
+import { VoltageChart } from './charts/voltage-chart'
+
+interface ChartsGridProps {
+  data: GetTelemetry200DataItem[] | undefined
+  isLoading: boolean
+}
+
+export function ChartsGrid({ data, isLoading }: ChartsGridProps) {
+  return (
+    <div className="grid gap-4 md:grid-cols-2">
+      <VoltageChart data={data} isLoading={isLoading} />
+      <CurrentChart data={data} isLoading={isLoading} />
+      <PowerChart data={data} isLoading={isLoading} />
+      <PowerFactorChart data={data} isLoading={isLoading} />
+    </div>
+  )
+}
